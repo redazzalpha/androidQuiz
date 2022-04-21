7815ae66e9fa36327ac47260b31abb63cd6d0081
kyzer <redazzkillah@gmail.com>
2022-04-21 23:38:42 +0200

    add first activity

--- app/src/main/java/com/example/quizexo/MainActivity.java
+++ app/src/main/java/com/example/quizexo/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.quizexo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.quizexo.dao.ChoiceDao;
 import com.example.quizexo.dao.QuestionDao;
@@ -17,17 +24,65 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private EditText inputName;
+    private Button btnStart;
+    private ImageButton btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get components
+        inputName = findViewById(R.id.inputName);
+        btnStart = findViewById(R.id.btnStart);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        // set listeners
+        inputName.addTextChangedListener(new inputNameOnValueChange());
+        btnDelete.setOnClickListener(this::clearInputName);
+        btnStart.setOnClickListener(this::startQuiz);
         executor.execute(this::createDatabase);
 
+        disableButtons();
+    }
+
+    public void clearInputName(View view) {
+        this.inputName.setText("");
+    }
+    public void startQuiz(View view) {
+
+    }
+
+    public void enableStart() {
+        btnStart.setEnabled(true);
+        btnStart.setBackgroundColor(Color.parseColor("#95c500"));
+    }
+    public void enableDelete() {
+        btnDelete.setEnabled(true);
+        btnDelete.setColorFilter(Color.parseColor("#e90813"));
+    }
+    public void enableButtons() {
+        this.enableStart();
+        this.enableDelete();
+    }
+
+    public void disableStart() {
+        btnStart.setEnabled(false);
+        btnStart.setBackgroundColor(Color.GRAY);
+    }
+    public void disableDelete() {
+        btnDelete.setEnabled(false);
+        btnDelete.setColorFilter(Color.GRAY);
+    }
+    public void disableButtons() {
+        this.disableStart();
+        this.disableDelete();
     }
 
     public void createDatabase() {
@@ -54,8 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
         i = 1;
         for(String choice : Defines.choices){
-            if(!choice.equals("\n"))
-                choices.add(new Choice(j++, i, choice));
+            if(!choice.equals("\n")) choices.add(new Choice(j++, i, choice));
             else i++;
         }
 
@@ -66,5 +120,27 @@ public class MainActivity extends AppCompatActivity {
         List<QuestionChoices> l = questionDao.getAllQuestionChoices();
         for(QuestionChoices c : l)
             System.out.println("****************************************** " + c.question + " - " + c.choices);
+    }
+
+    class inputNameOnValueChange implements TextWatcher {
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String username = editable.toString().trim();
+            Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\s]+(-[a-zA-Z0-9\\s]+)*$");
+            Matcher matcher = pattern.matcher(username);
+            boolean empty = username.isEmpty();
+
+            if(!empty && matcher.find()) enableButtons();
+            else if(empty) disableButtons();
+            else disableStart();
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
     }
 }
--- app/src/main/java/com/example/quizexo/defines/Defines.java
+++ app/src/main/java/com/example/quizexo/defines/Defines.java
@@ -36,6 +36,5 @@ public class Defines {
             "Inde",
             "Indonésie",
             "\n",
-
     };
 }
